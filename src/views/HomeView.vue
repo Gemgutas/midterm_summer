e9b7216f625f608bbfa230dbcac177db08dc9420
Gemmarie <Gemgutas>
2022-07-16 18:52:13 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,50 @@
+<template>
+  <h1>Home Page</h1>
+
+ 
+
+  <div class="card bg-light" style="width: 18rem; position:relative; left: 40%;">
+    <img class="card-img-top" src="..." alt="Card image cap" />
+    <div class="card-body">
+      <h5 class="card-title">User Information</h5>
+      <p class="card-text">
+        Lastname: {{ user.lname }}
+        <br />
+        Firstname: {{ user.fname }}
+        <br />
+        Mobile: {{ user.mobile }}
+        <br />
+
+        Course: {{ user.course }}
+        <br />
+        Year: {{ user.year }}
+        <br />
+      </p>
+      <p></p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useAuthStore } from "../stores/auth";
+import { useRouter } from "vue-router";
+
+export default {
+  setup() {
+    const { user, token } = useAuthStore();
+    const router = useRouter();
+
+    if (token == "") {
+      router.replace("/login");
+    }
+
+    return {
+      user,
+      token,
+    };
+  },
+};
+</script>
+
+<style></style>
